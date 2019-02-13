6fc42e9ecda267f058ac727054fd263ab0061478
Predrag Stosic <predrag@cloudtech.company>
2019-02-13 11:53:52 +0100

    configuring travis build & deployment

--- .build_scripts/build.sh
+++ .build_scripts/build.sh
@@ -3,6 +3,7 @@ set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Building site"
+  npm install -y
   gulp build
 else
   echo "Not building, so long and thanks for all the fish!"
