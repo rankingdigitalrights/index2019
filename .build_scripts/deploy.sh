23692a80f15ea8e106a070f94974bac80bcc54cc
Predrag Stosic <predrag@cloudtech.company>
2019-02-14 09:55:14 +0100

    configuring travis build & deployment

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -3,6 +3,7 @@ set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Deploying to Production"
+  scp -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* ranking5@rankingdigitalrights.org:/home4/ranking5/public_html/index2019/
 elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${STAGING_BRANCH} ]; then
   echo "Deploying to Staging"
   scp -P 2202 -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* rdr2019build@cloudtech.company:~/public_html
