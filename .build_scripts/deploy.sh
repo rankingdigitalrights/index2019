f1ef5aa8d2112b8e1b7663e79936a02dc11d0f81
Predrag Stosic <predrag@cloudtech.company>
2019-02-14 08:40:22 +0100

    configuring travis build & deployment

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = $DEPLOY_BRANCH ]; then
   echo "Deploying"
-  scp -P 2202 -i rdr2019build -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* rdr2019build@cloudtech.company:~/public_html
+  scp -P 2202 -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* rdr2019build@cloudtech.company:~/public_html
 else
   echo "Not a publishable branch so we're all done here"
 fi
