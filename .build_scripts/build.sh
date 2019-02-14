00e2cfffaede6076eed23f9aea790432efcdebaa
Predrag Stosic <predrag@cloudtech.company>
2019-02-14 09:22:54 +0100

    configuring travis build & deployment

--- .build_scripts/build.sh
+++ .build_scripts/build.sh
@@ -2,8 +2,11 @@
 set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
-  echo "Building site"
+  echo "Building site - Production"
   gulp prod
+elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${STAGING_BRANCH} ]; then
+  echo "Building site - Staging"
+  gulp stage
 else
   echo "Not building, so long and thanks for all the fish!"
 fi
--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,7 +2,9 @@
 set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = $DEPLOY_BRANCH ]; then
-  echo "Deploying"
+  echo "Deploying to Production"
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = $STAGING_BRANCH ]; then
+  echo "Deploying to Staging"
   scp -P 2202 -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* rdr2019build@cloudtech.company:~/public_html
 else
   echo "Not a publishable branch so we're all done here"
