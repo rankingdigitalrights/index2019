da21ca9a5b0d383ad2111e9ada29fb3e4950aa94
Predrag Stosic <predrag.stosic@clevertech.biz>
2019-02-15 23:59:11 +0100

    assets link bug fix

--- app/assets/scripts/util/base-url.js
+++ app/assets/scripts/util/base-url.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'development') {
   module.exports = 'http://localhost:3000';
-} else if (process.env.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'staging') {
   module.exports = 'http://rdr2019build.cloudtech.company';
 } else {
   module.exports = 'https://rankingdigitalrights.org/index2019';
