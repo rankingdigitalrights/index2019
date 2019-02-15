61322806dd14b4a38b6795e85f32a6e1d44f5fba
Predrag Stosic <predrag.stosic@clevertech.biz>
2019-02-15 11:23:36 +0100

    asseets links changes

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,10 +2,10 @@
 set -e # halt script on error
 
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
-  echo "Deploying to Production server"
+  echo "Deploying to Production"
   scp -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* ranking5@rankingdigitalrights.org:/home4/ranking5/public_html/index2019/
-elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEVELOPMENT_BRANCH} ]; then
-  echo "Deploying to Development server"
+elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${STAGING_BRANCH} ]; then
+  echo "Deploying to Staging"
   scp -P 2202 -i deploy_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp _site/* rdr2019build@cloudtech.company:~/public_html
 else
   echo "Not a publishable branch so we're all done here"
--- app/assets/scripts/util/base-url.js
+++ app/assets/scripts/util/base-url.js
@@ -1,2 +1,7 @@
-module.exports = process.env.NODE_ENV === 'development'
-  ? 'http://rdr2019build.cloudtech.company' : 'https://rankingdigitalrights.org/index2019';
+if (process.env.NODE_ENV === 'development') {
+  module.exports = 'http://localhost:3000';
+} else if (process.env.NODE_ENV === 'development') {
+  module.exports = 'http://rdr2019build.cloudtech.company';
+} else {
+  module.exports = 'https://rankingdigitalrights.org/index2019';
+}
