e9fd69675e95c6ae4cc724364526a7763c40efcc
Predrag Stosic <predrag.stosic@clevertech.biz>
2019-02-15 11:06:39 +0100

    Adding development environment in build/deploy process

--- .build_scripts/build.sh
+++ .build_scripts/build.sh
@@ -4,9 +4,9 @@ set -e # halt script on error
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Building site - Production"
   gulp prod
-elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${STAGING_BRANCH} ]; then
-  echo "Building site - Staging"
-  gulp stage
+elif [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEVELOPMENT_BRANCH} ]; then
+  echo "Building site - Development"
+  gulp dev
 else
   echo "Not building, so long and thanks for all the fish!"
 fi
