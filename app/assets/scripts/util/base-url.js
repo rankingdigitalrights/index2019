b7785f1576ad524655811f0f56a86af26ad9ec8e
Stevan Postic <stevan.postic@cloudtech.company>
2019-04-15 12:33:13 +0200

    base url

--- app/assets/scripts/util/base-url.js
+++ app/assets/scripts/util/base-url.js
@@ -1,3 +1,3 @@
 module.exports = process.env.NODE_ENV === 'development'
-  //? 'http://localhost:3000' : 'http://rdr2019build.cloudtech.company';
-  ? 'http://localhost:3000' : 'https://rankingdigitalrights.org/index2019';
+  ? 'http://localhost:3000' : 'http://rdr2019build.cloudtech.company';
+  // ? 'http://localhost:3000' : 'https://rankingdigitalrights.org/index2019';
